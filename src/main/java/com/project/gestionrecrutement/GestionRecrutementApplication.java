88fbbbbb71e2cc67af509f2d7ae3fff9047c1f52
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2024-12-27 21:35:02 +0100

    init project

---
+++ src/main/java/com/project/gestionrecrutement/GestionRecrutementApplication.java
@@ -0,0 +1,13 @@
+package com.project.gestionrecrutement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRecrutementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRecrutementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gestionrecrutement/GestionRecrutementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestionrecrutement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRecrutementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
